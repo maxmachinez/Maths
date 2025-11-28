96e5806c5ac3a2ea023a3d89ed53e86e968d5859
maxmachinez <maxayres347@gmail.com>
2025-11-28 10:12:19 +0000

    caesar - 2
    asd

--- caesar cipher.py
+++ caesar cipher.py
@@ -1,31 +1,38 @@
-#caesar cipher 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-
-def ceaserCipher(plaintext, key):
+ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+def caesarCipher(plaintext, key):
     plaintext = plaintext.lower()
     plaintext = plaintext.strip()
     ciphertext = ""
     for i in plaintext:
-        p = alphabet.index(i)
+        p = ALPHABET.index(i)
+        nP = (p + key) % 27 
         if i == " ":
             ciphertext += " "
-            nP = (p + key) % 27
-            ciphertext += alphabet[nP]
         elif p == " ":
-              p = alphabet.index(i + 1)
-              nP = (p + key) % 27
-              ciphertext += alphabet[nP]
+              p = ALPHABET.index(i + 1)
+              ciphertext += ALPHABET[nP]
         else:
-             nP = (p + key) % 27
-             ciphertext += alphabet[nP]
-
+             ciphertext += ALPHABET[nP]
     return ciphertext
-
-
-
-    
-
-plaintext =  str(input("What text do you want to encrypt?"))
-key = int(input("By what key do you want to encrypt by?"))
-
-print("This is your encrypted message: ", ceaserCipher(plaintext, key))
+def caesarDecrypter(cText, key):
+     cText.strip()
+     cText.lower()
+     plaintext = ""
+     for i in cText:
+          p = ALPHABET.index(i)
+          if i == " ":
+               plaintext += " "
+          else:
+               nP = (p - key) % 27
+               plaintext += ALPHABET[nP]
+     return plaintext
+choice = int(input("What do you want to do? Encrypt or Decrypt? (0 and 1 respectively):  "))
+match choice:
+     case 0:
+        plaintext =  str(input("What text do you want to encrypt?"))
+        key = int(input("By what key do you want to encrypt by?"))
+        print("This is your encrypted message: ", caesarCipher(plaintext, key))
+     case 1:
+        ciphertext = str(input("What is the ciphertext?"))
+        key = int(input("By what key do you want to shift back by?"))
+        print("Your decrypted message is:", caesarDecrypter(ciphertext, key))
