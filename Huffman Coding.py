71a9b05f640833b54e27feb69f6adc5503bb7b8b
maxmachinez <maxayres347@gmail.com>
2025-11-28 10:14:02 +0000

    huff code 2
    asd

--- Huffman Coding.py
+++ Huffman Coding.py
@@ -1,5 +1,4 @@
-#huffman coding
-
+"""#huffman coding
 text = str(input("Enter what you would like to encode:"))
 
 def getFrequencies(text): #gets all da frequencies of da letters
@@ -17,15 +16,95 @@ def sortList(frequencies):
     #frequencies.reverse()               # will rectify this later
     return frequencies
 
-
-def getTree(frequencies):
+def getTree(oList):
     tree = []
-    #frequencies.reverse()   # rectified so that we get the lowest value first
-    while len(frequencies) > 1:
-        for i in range(1, len(text)):
-            tree.append((frequencies[i], frequencies[i]))  # adding the least 2 
-            frequencies.pop(frequencies[i])    #removing the least 2                       #NOT WORKING  
-            frequencies.pop(frequencies[i+1])                                              #NOT WORKING    
-    return tree, frequencies
+    while len(oList) > 1: 
+        oList.sort(key=lambda x: x[1])  
+        tree += [oList[0:2]] 
+        combinedFreq = oList[0][1] + oList[1][1]  
+        combinedLetter = oList[0][0] + oList[1][0]
+        oList = oList[2:]  
+        oList.append((combinedLetter, combinedFreq))  
+        print(oList)
+        
+                                                          #  tempList = []
+                                                          #  fv = oList[i]
+                                                          #  print(fv)
+                                                          #  tempList.append(fv)                           #later
+                                                          #  try: tempList.append(oList[i + 1])
+                                                          #  except: break
+                                                          #  tree.append(tempList)
+    print(tree)
+getTree(sortList(getFrequencies(text)))
 
-print(getFrequencies(sortList(getTree(text))))
+def encode(frequency):
+    code = dict()
+    #for i in txt:
+    #    if i in code:
+    #         
+"""
+#huffman coding
+text = str(input("Enter what you would like to encode:"))
+
+def getFrequencies(text): #gets all da frequencies of da letters
+    frequencies = []
+    for i in text:
+        if (i, text.count(i)) not in frequencies: # idk why this is the only thing which works but ok
+            frequencies.append((i, text.count(i)))
+        if (i, text.count(i)) in frequencies:  # to avoid it going like [h, 1], [h, 2] ... etc
+            continue
+
+    return frequencies
+
+def sortList(frequencies):
+    frequencies.sort(key=lambda x: x[1]) # cool lambda thingy
+    #frequencies.reverse()               # will rectify this later
+    return frequencies
+
+def getTree(oList):
+    tree = []
+    bl = True
+    tmpAp = oList[0]
+    while len(oList) > 1: 
+        oList.sort(key=lambda x: x[1])  
+        tree += [oList[0:2]] 
+        combinedFreq = oList[0][1] + oList[1][1]  
+        combinedLetter = oList[0][0] + oList[1][0]
+        oList = oList[2:]  
+        oList.append((combinedLetter, combinedFreq))  
+        if bl:
+            tmpAp = combinedLetter, " ", combinedFreq, " - ", "1"
+            tree.append(tmpAp)
+            bl = False
+        if not bl:
+            tmpAp = combinedLetter, " ", combinedFreq, " - ", "0"
+            tree.append(tmpAp)
+        print(oList)
+
+        
+        
+                                                          #  tempList = []
+                                                          #  fv = oList[i]
+                                                          #  print(fv)
+                                                          #  tempList.append(fv)                           #later
+                                                          #  try: tempList.append(oList[i + 1])
+                                                          #  except: break
+                                                          #  tree.append(tempList)
+    print(tree)
+getTree(sortList(getFrequencies(text)))
+
+
+
+def encode(frequency):
+    code = dict()
+
+
+    
+
+                
+            
+        
+
+
+        
+
